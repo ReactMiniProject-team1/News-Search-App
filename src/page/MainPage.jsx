f2f995912349c3c9a3c4400fe59eea1c5b08964f
Yupmac <hohobanjang@gmail.com>
2022-08-25 21:52:30 +0900

    feat: add History.jsx

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import InputField from "../component/searchInput/InputField";
+
 
 export default function MainPage() {
-  return <h1>Welcome everyone!</h1>;
+  return (
+    <>
+      <h1>Welcome everyone!</h1>
+      <InputField />
+    </>
+  )
 }
