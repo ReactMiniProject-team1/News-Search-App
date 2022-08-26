47419eee4c260da901c29b3f456910b949857e0c
Dahye <06robin11@gmail.com>
2022-08-26 12:38:36 +0900

    .

--- src/component/header/BookmarkBtn.jsx
+++ src/component/header/BookmarkBtn.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components";
 import { BsFillBookmarkFill } from "react-icons/bs";
 
@@ -15,22 +14,3 @@ export default function Bookmark() {
     </BookmarkSt>
   );
 }
-=======
-import styled from 'styled-components';
-import { BsFillBookmarkFill } from 'react-icons/bs';
-
-  const BookmarkSt = styled.div`
-    position: absolute;
-    top: 2.5rem;
-    right: 7%;
-  `;
-
-  export default function Bookmark() {
-    return(
-        <BookmarkSt>
-            <BsFillBookmarkFill />
-        </BookmarkSt>
-    )
-
-  }
->>>>>>> 331065ae6c241a6c09bf7e6c56a4cbb7fcf96fc3
