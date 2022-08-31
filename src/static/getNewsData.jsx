e8c1656c9b6f5f5ccb86b3004f138ce665736b24
Yupmac <hohobanjang@gmail.com>
2022-08-31 15:37:33 +0900

    feat: edit API_Logic

--- src/static/getNewsData.jsx
+++ src/static/getNewsData.jsx
@@ -1,25 +1,23 @@
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
 const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
-export default async function getNewsData(query, page) {
+export default async function getNewsData(word, page) {
 
-  const URL = (query, page) => 
-    `${BASE_URL}?api-key=${API_KEY}&q=${query}&page=${page}&begin_date=19800101&sort=relevance`;
+  const URL = (word, page) => 
+    `${BASE_URL}?api-key=${API_KEY}&q=${word}&page=${page}&begin_date=19800101&sort=relevance`;
 
-  let result = await axios.get(URL(query, page), {
-    params: {
-      source: "The New York Times",
-      type_of_material: "News"
-    },
-    validateStatus: function (status) {
-      return status < 500;
-    }
-  })
-  .then(res => res.data.response.docs)
-  .catch(e => console.log(e))
-  
+  let result = await axios.get(URL(word, page)).then(res => res.data.response.docs);
+  result = result.map((each) => {
+    const {
+      headline: { main: title },
+      pub_date: date,
+      snippet: content,
+      web_url: url,
+      _id: id,
+    } = each;
+    return { title, date, content, url, id, clipped: false };
+  });
   return result;
 };
